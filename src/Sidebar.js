74084d2639e6efd920db33b70091f3a6e528ac81
jesusbryan99 <91440514+jesusbryan99@users.noreply.github.com>
2022-03-03 20:16:47 -0600

    Add files via upload

---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Sidebar.css';
+
+function Sidebar({ user }) {
+    return (
+        <div className="sidebar">
+         
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ src/Sidebar2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Sidebar2.css';
+
+function Sidebar2() {
+    return (
+        <div className="sidebar2">
+       
+        </div>
+    )
+}
+
+export default Sidebar2
